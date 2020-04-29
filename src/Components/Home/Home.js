5211eff3f1b876a6415532f9db233e0556dbb3d4
Biav <beatrizvieirasilva.v@gmail.com>
2020-04-28 22:09:06 -0300

    Components Style and variables

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import Footer from '../Footer/Footer';
+import Location from '../Information/Location/Location';
+import Realization from '../Information/Realization/Realization';
+import Stamp from '../Information/Stamp/Stamp';
+import Subscription from '../Subscription/Subscription';
+import Testimonials from '../Testimonials/Testimonials';
 
 const home = () => (
     <>
+        <Banner/>
+        <Testimonials/>
+        <Location/>
+        <Stamp/>
+        <Realization/>
+        <Subscription/>
         <Footer/>
     </>
 );
